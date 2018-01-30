e4b8622fcc3c2decb601b3f48d80b3242a0129ea
php 23 <php.23@zecolmeia.cec.ime.usp.br>
2018-01-30 21:44:54 -0200

    primeiro projeto laravel

---
+++ resources/views/saudacao.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title> Saudação</title>
+</head>
+<body>
+
+<?php for($i = 0; $i < 100; $i++): ?>
+<h1> Olá mundo php <?= $i ?></h1>
+<?php endfor; ?>
+</body>
+</html>
